26498f0045d2a20faf9e55b15eaf9891665b2f0b
yuexiaodong <xiaodong@mgtv.com>
2016-09-09 11:18:34 +0800

    templates

--- app/views.py
+++ app/views.py
@@ -1,18 +1,9 @@
 from app import app
-from flask.templating import render_template
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'raidyue'}
-    return '''
-        <html>
-            <head>
-                <title>Home Page</title>
-            </head>
-            <body>
-                <h1>Hello, %s</h1>
-            </body>
-        </html>
-    ''' % user['nickname']
+    return render_template('index.html', title='Home', user=user)
