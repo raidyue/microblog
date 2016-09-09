c3509c85f8b428641cb2b15b04f9a558b981d083
yuexiaodong <xiaodong@mgtv.com>
2016-09-09 17:00:33 +0800

    表单

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 
 @app.route('/')
@@ -21,4 +22,21 @@ def index():
         title='Home',
         user=user,
         posts=posts
+    )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            'Ligin requested for OpenID=%s, remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data))
+        )
+        return redirect('/index')
+    return render_template(
+        'login.html',
+        title='Sign In',
+        form=form,
+        providers=app.config['OPENID_PROVIDERS']
     )
