23cad5521447df71c2b4000d80f53b9dbae03ebb
yuexiaodong <xiaodong@mgtv.com>
2016-09-09 11:21:43 +0800

    模版循环

--- app/views.py
+++ app/views.py
@@ -6,4 +6,19 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'nickname': 'raidyue'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template(
+        'index.html',
+        title='Home',
+        user=user,
+        posts=posts
+    )
