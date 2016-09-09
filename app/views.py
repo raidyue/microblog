a295c5b027099816419ed7b6082fa2811198dfd8
yuexiaodong <xiaodong@mgtv.com>
2016-09-09 10:50:20 +0800

    return html

--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from app import app
+from flask.templating import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {'nickname': 'raidyue'}
+    return '''
+        <html>
+            <head>
+                <title>Home Page</title>
+            </head>
+            <body>
+                <h1>Hello, %s</h1>
+            </body>
+        </html>
+    ''' % user['nickname']
